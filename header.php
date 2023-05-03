244ba59e915d009a8f18e57d41d72a248a26d357
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-03 16:36:20 +0200

    Include a PHP file in another file

---
+++ header.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Display Names</title>
+    
+    <style>
+        .some-names {
+        background-color: navy;
+        color: white;
+        }
+    
+        .more-names {
+            background-color: gray;
+        }
+    </style>
+</head>
--- names.php
+++ names.php
@@ -4,23 +4,9 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Display Names</title>
 
-    <style>
-        .some-names {
-            background-color: navy;
-            color: white;
-        }
+<?php include('./header.php') ?>
 
-        .more-names {
-            background-color: gray;
-        }
-    </style>
-</head>
 <body>
     <h1>List of names :</h1>
     <ul class="<?= count($names) > 3 ? 'more-names' : 'some-names' ?>">
