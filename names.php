32a7ceda7cc4c8a33bbaba0acf30709b29673c8f
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-03 16:17:15 +0200

    Dynamic change of the background color of the list of names according to the number of elements in the array of names

--- names.php
+++ names.php
@@ -9,10 +9,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Display Names</title>
+
+    <style>
+        .some-names {
+            background-color: navy;
+            color: white;
+        }
+
+        .more-names {
+            background-color: gray;
+        }
+    </style>
 </head>
 <body>
     <h1>List of names :</h1>
-    <ul>
+    <ul class="<?= count($names) > 3 ? 'more-names' : 'some-names' ?>">
         <?php foreach($names as $name) { ?>
             <li><?= $name ?> </li>
         <?php } ?>
